bac3c4fcd44ed385e2798ae55586da8ac58a0103
Sung <slee1070@my.bcit.ca>
2023-03-11 22:06:36 -0800

    Draw chart from firestore data

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,47 @@
-const ctx = document.getElementById('myChart');
+let userID;
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+jQuery(async function() {
+    // On page load
+    const app = firebase.app();
+
+    app.auth().onAuthStateChanged((user) => {
+        if (user) {
+          userID = user.uid;
         }
+    });
+
+    let results = await getResultForCity('Vancouver');
+
+    drawChart(results);
+});
+
+async function getResultForCity(city) {
+  return db.collection("results").doc(city).get().then((doc) => {
+      if (doc.exists) {
+          return doc.data();
+      }
+  });
+}
+
+function drawChart(results) {
+  const chartElement = $('#result-chart');
+
+  new Chart(chartElement, {
+      type: 'bar',
+      options: {
+        indexAxis: 'y',
+        // Elements options apply to all of the options unless overridden in a dataset
+        // In this case, we are setting the border of each horizontal bar to be 2px wide
+      },
+      data: {
+        labels: Object.keys(results), // language
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: Object.values(results) // number of votes
+          }
+        ]
       }
     }
-  });
+  );
+}
